a3395fc56e2a314cf3278e8e08852846b1daa95d
rahul <rahulpwar654@gmail.com>
2024-02-07 13:58:59 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+class Employee(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    salary: float
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
